9cd08acd3eda2690b175a54bedeb0154ef9db36d
mhselim103 <mhselin103@gmail.com>
2021-11-02 16:06:44 +0600

    almost done

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -1,26 +1,56 @@
 import React from "react";
 import "./Footer.css";
 import footerLogo from "../../../images/logo.png";
-import { Col, Row } from "react-bootstrap";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="content">
-        <footer className="container">
-          <Row>
-            <Col className="col-md-6 ">
-              <div className=" d-flex flex-column align-content-center">
-                <div>
-                  <img className="w-25" src={footerLogo} alt="" />
-                </div>
-                <div>
-                  <p>Developed By @Mh-Selim</p>
-                </div>
+      <div className="container p-5">
+        <div className="row text-light">
+          <div className="col-md-6 left-container">
+            <div>
+              <img className="w-25" src={footerLogo} alt="" />
+            </div>
+          </div>
+          <div className="col-md-6 right-container">
+            <div className="row">
+              <div className="col-md-6 d-md-flex justify-content-md-center justify-content-sm-center">
+                <ul className="">
+                  <li className="footer-menu">About Online Food</li>
+                  <li className="footer-menu">Read our blog</li>
+                  <li className="footer-menu">Sign up to deliver</li>
+                  <li className="footer-menu">Add your restaurant</li>
+                  <li className="footer-menu">Get Help</li>
+                  <li className="footer-menu">Ask any question</li>
+                </ul>
               </div>
-            </Col>
-          </Row>
-        </footer>
+              <div className="col-md-6 d-md-flex justify-content-md-center justify-content-sm-center">
+                <ul className="">
+                  <li className="footer-menu">Facebook</li>
+                  <li className="footer-menu">Instagram</li>
+                  <li className="footer-menu">Whatsapp</li>
+                  <li className="footer-menu">Youtube</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row text-light d-md-flex align-items-md-center">
+          <div className="col-md-6">
+            <div className="d-md-flex justify-content-md-start">
+              <p>all right reserved by mh selim</p>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div>
+              <ul className="d-flex justify-content-evenly">
+                <li className="footer-menu">Privacy Policy</li>
+                <li className="footer-menu">Terms and Policy</li>
+                <li className="footer-menu">Pricing</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
